37528153981b8c6c8c407b2a18196b70b029f100
lenhu97 <44870748+lenhu97@users.noreply.github.com>
2020-01-06 19:28:44 +0700

    sssss

--- daugiatructuyen/utils/db.js
+++ daugiatructuyen/utils/db.js
@@ -2,20 +2,20 @@ const mysql = require('mysql');
 const util = require('util');
 
 const pool = mysql.createPool({
-  connectionLimit: 50,
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',
-  password: '123456',
-  database: 'daugia'
+    connectionLimit: 50,
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: '123456',
+    database: 'daugia'
 });
 
 const mysql_query = util.promisify(pool.query).bind(pool);
 
 module.exports = {
-  load: sql => mysql_query(sql),
-  add: (tableName, entity) => mysql_query(`insert into ${tableName} set ?`, entity),
-  del: (tableName, condition) => mysql_query(`delete from ${tableName} where ?`, condition),
-  patch: (tableName, entity, condition) => mysql_query(`update ${tableName} set ? where ?`, [entity, condition]),
-  
+    load: sql => mysql_query(sql),
+    add: (tableName, entity) => mysql_query(`insert into ${tableName} set ?`, entity),
+    del: (tableName, condition) => mysql_query(`delete from ${tableName} where ?`, condition),
+    patch: (tableName, entity, condition) => mysql_query(`update ${tableName} set ? where ?`, [entity, condition]),
+
 };
